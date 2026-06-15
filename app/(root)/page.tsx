48b4b17d768383036e683c829a0f0725e9fc326c
chinonsoxavier <chinonsoxavier26@gmail.com>
2026-06-15 18:42:39 +0100

    starter

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "@/components/layouts/main_layout";
+import React from "react";
+
+const Page = () => {
+  return (
+    <MainLayout>
+      <div>Page</div>
+    </MainLayout>
+  );
+};
+
+export default Page;
